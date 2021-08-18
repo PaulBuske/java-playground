e37071557b3235114547faa6c00ece2e5a387f01
PaulBuske <paul.a.buske.mil@swf.army.mil>
2021-08-18 14:36:31 -0500

    another update

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,16 +3,7 @@ package com.galvanize;
 public class Application {
     public static void main(String[] args) {
        System.out.println("hello world");
-
-
-       int x = 5;
-       switch (x) {
-           case 5:
-               System.out.println("Apples");
-               break;
-           case 4: System.out.println("bananas");
-           break;
+        Program.sayHello();
        }
 
     }
-}
--- src/main/java/com/galvanize/Program.java
+++ src/main/java/com/galvanize/Program.java
@@ -2,6 +2,10 @@ package com.galvanize;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("It works!!");
+//        System.out.println("It works!!");
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello!");
     }
 }
