102d135e40924c7f4ab7bd11b1175992baa73363
PaulBuske <paul.a.buske.mil@swf.army.mil>
2021-08-18 13:50:07 -0500

    Added jar's and stuff

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -2,6 +2,17 @@ package com.galvanize;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+       System.out.println("hello world");
+
+
+       int x = 5;
+       switch (x) {
+           case 5:
+               System.out.println("Apples");
+               break;
+           case 4: System.out.println("bananas");
+           break;
+       }
+
     }
 }
