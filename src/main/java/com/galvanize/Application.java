96dd14db212d23d1113a93ce03f11b1ea857df4a
PaulBuske <paul.a.buske.mil@swf.army.mil>
2021-08-19 16:31:13 -0500

    syncing for end of day

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -2,17 +2,45 @@ package com.galvanize;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello world");
+       /* System.out.println("hello world");
         Program.sayHello();
-    }
 
-    int number;
-    int print;
-    print = 45;
-do while ((print>=45) && (print<=50)){
+         int number = 45;
+        int count = 45;
 
-    }
+        while( count>=45 && count<=50)  {
+
+            System.out.println(number);
+            count++;
+            number++;
+        }*/
+
+
+//        for (int count = 2; count >= 2 && count <= 6; count++) {
+//            {
+//                if (count % 2 == 0) {
+//                    System.out.println(count);
+//                }
+//                count++;
+//            }
+//        }
 }
+// class BankAccount
+//{
+//    private String name;
+//
+//    private Float balance;
+//
+//
+//}
+
+}
+
+
+
+
+
+
 
 
 
